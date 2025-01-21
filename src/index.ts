079effcec5ce65463e864d2c71d4c109c0e56c93
krassowski <5832902+krassowski@users.noreply.github.com>
2025-01-21 10:58:37 +0000

    More work on toolbar integration

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,60 @@ import {
 import { INotebookTracker } from '@jupyterlab/notebook';
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
 import { ITranslator, nullTranslator } from '@jupyterlab/translation';
-import { bellIcon, editIcon } from '@jupyterlab/ui-components';
+import { LabIcon } from '@jupyterlab/ui-components';
 import {
   createDefaultFactory,
   IToolbarWidgetRegistry,
 } from '@jupyterlab/apputils';
+import { bellOutlineIcon, bellFilledIcon, bellOffIcon, bellAlertIcon } from './icons';
+
+
 namespace CommandIDs {
   export const toggleCellNotifications = 'toggle-cell-notifications';
 }
 
 const CELL_METADATA_KEY = 'jupyterlab_notify.notify';
 
+
+interface IMode {
+  label: string;
+  icon: LabIcon;
+}
+
+const ModeIds = ['always', 'never', 'on-error', 'global-timeout', 'custom-timeout'] as const;
+type ModeId = typeof ModeIds[number];
+
+const MODES: Record<ModeId, IMode> = {
+  'always': {
+    label: 'Always',
+    icon: bellFilledIcon
+  },
+  'never': {
+    label: 'Never',
+    icon: bellOffIcon
+  },
+  'on-error': {
+    label: 'On error',
+    icon: bellAlertIcon
+  },
+  'global-timeout': {
+    label: 'If longer than global timeout',
+    icon: bellOutlineIcon
+  },
+  'custom-timeout': {
+    label: 'If longer than %1',
+    icon: bellOutlineIcon // TODO: custom icon with a tiny clock
+  }
+}
+
+
+interface ICellMetadata {
+  mode: ModeId;
+  timeoutSeconds?: number;
+}
+
 /**
- * Initialization data for the {{ labextension_name }} extension.
+ * Initialization data for the jupyterlab-notify extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-notify:plugin',
@@ -33,9 +74,11 @@ const plugin: JupyterFrontEndPlugin<void> = {
     settingRegistry: ISettingRegistry | null,
   ) => {
     console.log('JupyterLab extension jupyterlab-notify is activated!');
+    // TODO make it customizable
+    const defaultMode = 'global-timeout';
 
     const trans = (translator ?? nullTranslator).load('jupyterlab-notify');
-    app.commands.addCommand(CommandIDs.toggleCellNotifications, {
+      app.commands.addCommand(CommandIDs.toggleCellNotifications, {
       label: args => {
         const current = tracker.currentWidget;
         return trans._n(
@@ -53,23 +96,14 @@ const plugin: JupyterFrontEndPlugin<void> = {
           return;
         }
         for (const cell of current.content.selectedCells) {
-          const mode = cell.model.getMetadata(CELL_METADATA_KEY);
-          // TODO: define modes, say: never/always/on failure/after timeout
-          // TODO: add a dropdown to select timeout
-          // TODO: add different variants of the icon, ideally a smaller icon
-          let newMode: string;
-          switch (mode) {
-            case 'always':
-              newMode = 'never';
-              break;
-            case 'never':
-              newMode = 'always';
-              break;
-            default:
-              newMode = 'always';
-              break;
+          const oldMetadata = cell.model.getMetadata(CELL_METADATA_KEY) as ICellMetadata | undefined;
+          const oldModeId = oldMetadata?.mode ?? defaultMode;
+          let nextModeIndex = ModeIds.indexOf(oldModeId) + 1;
+          if (nextModeIndex >= ModeIds.length) {
+            nextModeIndex = 0;
           }
-          cell.model.setMetadata(CELL_METADATA_KEY, newMode);
+          const newModeId = ModeIds[nextModeIndex];
+          cell.model.setMetadata(CELL_METADATA_KEY, {mode: newModeId, ...oldMetadata});
           app.commands.notifyCommandChanged(CommandIDs.toggleCellNotifications);
         }
       },
@@ -82,22 +116,24 @@ const plugin: JupyterFrontEndPlugin<void> = {
           return undefined;
         }
         const cell = current.content.selectedCells[0];
-        const mode = cell.model.getMetadata(CELL_METADATA_KEY);
-        if (mode === 'always') {
-          return editIcon;
-        }
-        return bellIcon;
+        const metadata = cell.model.getMetadata(CELL_METADATA_KEY) as ICellMetadata | undefined;
+        const modeId = metadata?.mode ?? defaultMode;
+        const mode = MODES[modeId];
+        return mode.icon;
       },
       isEnabled: args => (args.toolbar ? true : !!tracker.currentWidget),
     });
 
     if (toolbarRegistry) {
+      // TODO: add a dropdown to select timeout
       const itemFactory = createDefaultFactory(app.commands);
       toolbarRegistry.addFactory('Cell', 'notify', widget => {
-        return itemFactory('Cell', widget, {
+        const toolbarButton = itemFactory('Cell', widget, {
           name: 'notify',
           command: CommandIDs.toggleCellNotifications,
         });
+        // const dropDownButton = new
+        return toolbarButton
       });
     }
 
